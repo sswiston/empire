ff042327e7133247d64beecc7168b9b35005546e
Swiston <k.swiston@compute1-client-1.ris.wustl.edu>
2025-05-06 13:23:31 -0500

    edits to cluster scripts

--- cluster_scripts/run_rase_job.sh
+++ cluster_scripts/run_rase_job.sh
@@ -1,6 +1,6 @@
 # Set up volumes
-LSF_DOCKER_VOLUMES="/storage1/fs1/michael.landis/Active/ellipses:/storage1/fs1/michael.landis/Active/ellipses"
-JOBDIR="/storage1/fs1/michael.landis/Active/ellipses/joblogs"
+LSF_DOCKER_VOLUMES="/storage1/fs1/michael.landis/Active/empire:/storage1/fs1/michael.landis/Active/empire"
+JOBDIR="/storage1/fs1/michael.landis/Active/empire/joblogs"
 
 # Set up analysis
 if [ $# == 2 ]; then
@@ -12,9 +12,9 @@ fi
 # Submit job
 bsub -G compute-michael.landis \
 -g /k.swiston/ellipses \
--cwd /storage1/fs1/michael.landis/Active/ellipses/ \
+-cwd /storage1/fs1/michael.landis/Active/empire/ \
 -o $JOBDIR/$NAME.stdout.txt \
 -J $NAME \
 -q general \
 -n 1 -M 4GB -R "rusage [mem=4GB] span[hosts=1]" \
--a 'docker(sswiston/ellipses:2)' /bin/bash /storage1/fs1/michael.landis/Active/ellipses/cluster_scripts/run_rase.sh $NUMBER $CONDITION
+-a 'docker(sswiston/ellipses:2)' /bin/bash /storage1/fs1/michael.landis/Active/empire/cluster_scripts/run_rase.sh $NUMBER $CONDITION
